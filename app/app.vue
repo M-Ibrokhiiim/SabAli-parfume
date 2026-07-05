a7691923239877a92a77ca488ce6947f2a886e89
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-07-05 19:25:51 +0500

    Fully finished DEMO

--- app/app.vue
+++ app/app.vue
@@ -4,3 +4,13 @@
         <NuxtPage/>
     </NuxtLayout>    
 </template>
+
+<style>
+/* Global stylesheet to override the default browser background canvas */
+html,
+body {
+  background-color: #000000 !important;
+  color: #ffffff;
+  min-height: 100vh;
+}
+</style>
